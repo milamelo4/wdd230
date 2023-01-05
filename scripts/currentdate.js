05871d17da6f270ccdb00a739c3dd7e3f4cf2da5
milamelo4 <mel19009@byui.edu>
2023-01-05 13:23:06 -0700

    Update
    Updated some html.
    Fixed the current date in js.
    Updated css

--- scripts/currentdate.js
+++ scripts/currentdate.js
@@ -1,26 +1,7 @@
-// const options = {
-//   month: 'short', day: 'numeric', year: 'numeric'
-// };
-// const newdate = new Date().toLocaleDateString('en-us', options);
-// console.log(newdate)
+// Getting the current year
+const date1 = document.querySelector("#date1");
+date1.innerHTML = new Date().getFullYear();
 
-// document.getElementById('date').innerHTML = `Last Updated: ${newdate}`
-
-//alert(document.lastModified);
-// returns: Tuesday, December 16, 2017 11:09:42
-
-let oLastModif = new Date(document.lastModified);
-let nLastModif = Date.parse(document.lastModified);
-const lastVisit = parseFloat(document.cookie.replace(/(?:(?:^|.*;)\s*last_modif\s*=\s*([^;]*).*$)|^.*$/, "$1"));
-const lastModif = Date.parse(document.lastModified);
-
-if (isNaN(lastVisit) || lastModif > lastVisit) {
-  document.cookie = `last_modif=${Date.now()}; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=${location.pathname}`;
-
-  if (isFinite(lastVisit)) {
-    alert("This page has been changed!");
-  }
-}
-
-document.getElementById('date').innerHTML = `Last Updated: ${oLastModif}`
-
+// Getting the last modified date
+const date2 = document.querySelector("#date2");
+date2.innerHTML = new Date(document.lastModified).toLocaleDateString("en-US");
