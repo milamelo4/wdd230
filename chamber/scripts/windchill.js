195b056445317f94d81bfc21fe54d01ee36350fc
milamelo4 <mel19009@byui.edu>
2023-02-10 17:17:44 -0700

    Final changes, deleted unnecessary stuff

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -4,7 +4,7 @@ let windSpeed = parseFloat(document.querySelector('#windSpeed').textContent);
 
 if (temperature <= 50 && windSpeed > 3) {
     let windChill = 35.74 + 0.6215 * temperature - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temperature * Math.pow(windSpeed, 0.16);
-    document.querySelector('#windChill').innerHTML = `Wind Chill: ${windChill.toFixed(2)} °F`;
+    document.querySelector('#windChill').innerHTML = `Wind Chill: ${windChill.toFixed(2)}°F`;
  
 }
 else {
