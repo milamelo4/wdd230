6d7db09aa0430af8c53ab576be921693fba5cfc8
milamelo4 <mel19009@byui.edu>
2023-02-24 19:31:53 -0700

    finished join and thankyou page

--- chamber/scripts/date.js
+++ chamber/scripts/date.js
@@ -1,4 +1,13 @@
 //Thank you time
-
 let subTime = document.getElementById('dateTime');
 subTime.innerHTML = date.toLocaleString();
+
+//Get user's first name and last from the form. Since we use a method action 'get' on the form we can search any info entered that shows up un the URL window.Then use the method URLSearchParams.
+let fname = document.getElementById('fname');
+
+
+const params = new
+URLSearchParams(window.location.search)
+const naMe = params.get('fname')
+const last = params.get('lname')
+fname.innerHTML = `${naMe} ${last},`
