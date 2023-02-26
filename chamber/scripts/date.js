0f4f363f2d8dddfbccd3cc03f72bf3c7427d47f0
milamelo4 <mel19009@byui.edu>
2023-02-26 15:11:06 -0700

    Added a value for membership

--- chamber/scripts/date.js
+++ chamber/scripts/date.js
@@ -1,13 +1,16 @@
 //Thank you time
 let subTime = document.getElementById('dateTime');
-subTime.innerHTML = date.toLocaleString();
+subTime.innerHTML = `${date.toLocaleString()}.`;
 
 //Get user's first name and last from the form. Since we use a method action 'get' on the form we can search any info entered that shows up un the URL window.Then use the method URLSearchParams.
 let fname = document.getElementById('fname');
+let memberships = document.getElementById('member')
 
 
 const params = new
 URLSearchParams(window.location.search)
 const naMe = params.get('fname')
 const last = params.get('lname')
-fname.innerHTML = `${naMe} ${last},`
+const member = params.get('membership')
+fname.innerHTML = `${naMe} ${last}, `
+memberships.innerHTML = `${member}.`
