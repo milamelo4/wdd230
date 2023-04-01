3ecb2d2ccff61790c97d096ac15ad7c9639cb601
milamelo4 <mel19009@byui.edu>
2023-04-01 14:04:20 -0600

    add lazy loading w/ intersection obs

--- final/scripts/app.js
+++ final/scripts/app.js
@@ -31,3 +31,39 @@ function toggleMenu() {
 }
 const x = document.getElementById("hamburgerBtn");
 x.onclick = toggleMenu;
+
+// Get all images with data-src attribute. The 'img[data-src]' will look at ever img that has the data-src attribute. Anything inside a bracket means is looking for a attribute.
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+
+//Optional parameter for IntersectionalObserver
+const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 50px 0px"
+};
+
+const loadImages = (image) => {
+    image.setAttribute("src", image.getAttribute("data-src"));
+    image.onload = () => {
+        image.removeAttribute("data-src")
+    };
+};
+
+// Check if Intersection Observer is supported
+if ("IntersectionObserver" in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+        items.forEach(item => {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
+            }
+        });
+    }, imgOptions);
+
+    imagesToLoad.forEach(img => {
+        observer.observe(img)
+    });
+}else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    })
+}
